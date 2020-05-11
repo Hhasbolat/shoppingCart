385439928cd485d68aab60d83c298b5290645770
Halit Can Hasbolat <hhasbolat@ebay.com>
2020-05-11 11:49:39 +0300

    delete commandLiner

--- src/main/java/com/trendyol/shoppingcart/ShoppingCartApplication.java
+++ src/main/java/com/trendyol/shoppingcart/ShoppingCartApplication.java
@@ -1,20 +1,12 @@
 package com.trendyol.shoppingcart;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
-@Transactional
-public class ShoppingCartApplication implements CommandLineRunner{
+public class ShoppingCartApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
     }
 }
